c6c24e72274d177e96ffe227c9e3893f99e26242
Saurav s Mehta <suaravmehta631@gmail.com>
2025-07-03 17:38:00 +0530

    sub string position

--- string/searchSub.c
+++ string/searchSub.c
@@ -1,57 +1,36 @@
-//WAP to check if a substring is in  string or not
-
 #include<stdio.h>
 #include<string.h>
-
 int main(){
+    char str[50], sub[20];
 
-    char str[50],subStr[50];
-    printf("enter string: ");
-    gets(str);
-    int i = 0,j = 0,subLength;
-    int length = strlen(str);
-       
-    printf("enter sub String: ");
-    gets(subStr);
-    
-    subLength = strlen(subStr);
+    int l = strlen(str);
+    int sl = strlen(sub);
 
-    int flag = 1,b = 1;
+    int i = 0, j,k,found = 0;
 
-    while(subLength>0 && str[j]!='\0'){
-        if(subStr[i]==str[j]){
-            flag = 1;
-            subLength--;
-            i++;
-            j++;
-            while(subLength>0 && str[j]!='\0')
-            {
-                if(subStr[i]!=str[j]){
-                    flag = 0;
-                    b = 0;
-                    break;
-                }
-                subLength--;
-                i++;
-                j++;
-            }
-        }
-        else{
-            flag = 0;
+    printf("enter string: ");gets(str);
+    printf("enter sub string: ");gets(sub);
+
+    while(str[i]!='\0')
+    {
+        k = i,j = 0;
+        while(str[k]==sub[j] && sub[j]!='\0'){
+            k++;
             j++;
         }
-
-        if(b ==0){
+        if(sub[j]=='\0'){
+            found = 1;
             break;
         }
+        i++;
+    }
+    if(found){
+        printf("found at position %d of string",i);
+    }
+    else{
+        printf("not found");
     }
 
-
-    if(flag == 1){
-        printf("found");
-    }else{
-        printf("substring not found");
-    }
-    
     return 0;
+
 }
