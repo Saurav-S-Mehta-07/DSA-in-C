f469c1eba28cca9220aa53007c0da1e55b6949b5
Saurav s Mehta <suaravmehta631@gmail.com>
2025-07-05 18:58:31 +0530

    added

--- string/replace.c
+++ string/replace.c
@@ -1,9 +1,7 @@
 #include<stdio.h>
 int main(){
     char str[200],pat[20],new_str[200],rep_pat[100];
-
     int i = 0, j= 0, k,  n=0, copy_loop = 0, rep_index = 0;
-
     printf("enter string: ");gets(str);
     printf("enter sub string: ");gets(pat);
     printf("enter replacing pattern: ");gets(rep_pat);
